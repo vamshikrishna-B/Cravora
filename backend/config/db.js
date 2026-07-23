28207260e198f094bec735cfefc6c26dbd02f108
vamshi krishna -B <bvamshikrishna1912@gmail.com>
2026-07-23 12:48:20 +0530

    Remove backend directory from repo (keep local)

--- backend/config/db.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-  await mongoose
-    .connect(
-      process.env.MONGO_URL
-    )
-    .then(() =>console.log("DB Connected"));
-};
